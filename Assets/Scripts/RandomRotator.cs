9fa8eb36cd04cfd10da9a44033c1c002d70e277d
ApiO <ac_flood@hotmail.fr>
2015-08-28 14:01:45 +0200

    Step 2.3 done

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour 
+{
+	public float tumble;
+
+	private Rigidbody _rigidBody;
+	
+	void Start()
+	{
+		_rigidBody = GetComponent<Rigidbody>();
+		_rigidBody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
